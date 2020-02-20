0265048ddd66bcc87a442731ad3432ce5180f436
IsleBeeGun <IsleBeeGun@gmail.com>
2020-02-21 02:13:20 +0300

    Add FormComponent loops (later fix additional field)

--- src/Components/FormComponent/FormComponent.jsx
+++ src/Components/FormComponent/FormComponent.jsx
@@ -15,23 +15,70 @@ export class FormComponent extends React.Component {
             <span className="col">{this.props.title}</span>
           </div>
           <div className="row">
-            <span className="col">multiple fields</span>
+            {/* Here comes the main group of inputs, it's nested in a column */}
+            <div className={this.props.field_groups.main}>
+              <div className="row">
+                {main.map((field, index) => {
+                  return (
+                    <div
+                      className="form-group col-md-6"
+                      key={index + "-" + field.name}
+                    >
+                      <label htmlFor={index + "-" + field.name}>
+                        {field.label}
+                      </label>
+                      <input
+                        type={field.type}
+                        id={index + "-" + field.name}
+                        name={field.name}
+                        required={field.required}
+                        className="form-control"
+                      />
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+            {/* Here comes the additional group of inputs in column (right now only single entry in page.json) */}
+            <div className={this.props.field_groups.additional}>
+              <div className="row h-75">
+                {additinal.map((field, index) => {
+                  return (
+                    <div
+                      className="form-group col"
+                      key={index + "-" + field.name}
+                    >
+                      <label htmlFor={index + "-" + field.name}>
+                        {field.label}
+                      </label>
+                      <input
+                        type={field.type}
+                        id={index + "-" + field.name}
+                        name={field.name}
+                        required={field.required}
+                        className="form-control w-100 h-75"
+                      />
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
           </div>
           {/* Here GDPR checkbox is added (right now there is only one entry in page.json) */}
           {gdpr.map((field, index) => {
             return (
-              <div className="row" key={field.name+"-"+index}>
+              <div className="row" key={index + "-" + field.name}>
                 <div className="col-auto">
                   <div className="custom-control custom-checkbox">
                     <input
                       type={field.type}
                       name={field.name}
-                      id={field.name+"-"+index}
+                      id={index + "-" + field.name}
                       required={true}
                       className="custom-control-input"
                     />
                     <label
-                      htmlFor={field.name+"-"+index}
+                      htmlFor={index + "-" + field.name}
                       className="custom-control-label"
                     >
                       {require("html-react-parser")(field.label)}
@@ -43,7 +90,7 @@ export class FormComponent extends React.Component {
           })}
 
           <div className="row">
-            <button type="button" className="col-auto btn btn-success btn-lg">
+            <button type="submit" className="col-auto btn btn-success btn-lg">
               {this.props.submit_button.text}
             </button>
           </div>
