c8fa811f068cc1f36017b00c1defd6e36c97b274
IsleBeeGun <IsleBeeGun@gmail.com>
2020-02-23 22:35:38 +0300

    Uninstall @fortawesome (too many packages)

--- src/Components/SliderComponent/SliderComponent.jsx
+++ src/Components/SliderComponent/SliderComponent.jsx
@@ -1,18 +1,44 @@
 import React from "react";
 import "./SliderComponent.css";
 import Slider from "react-slick";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faArrowAltCircleRight,
+  faArrowAltCircleLeft
+} from "@fortawesome/free-regular-svg-icons";
 
-function NextArrow(props) {
-  const { className, style, onClick } = props;
-  return (
-    <button className={className + " arrow-alt-circle-right"} style={{ ...style, backgroundColor: "black", content: "h" }} onClick={onClick} />
-  )
-}
-function PrevArrow(props) {
-  const { className, style, onClick } = props;
-  return <button className={className} style={{ ...style, backgroundColor: "black"  }} onClick={onClick} />;
-}
+// function PrevArrow(props) {
+//   const { className, style, onClick } = props;
+//   return (
+//     <button
+//       className={className}
+//       style={{ ...style, backgroundColor: "maroon" }}
+//       onClick={onClick}
+//     />
+//   );
+// }
+// function NextArrow(props) {
+//   const { className, style, onClick } = props;
+//   return (
+//     <button
+//       className={className}
+//       style={{ ...style, backgroundColor: "green" }}
+//       onClick={onClick}
+//     />
+//   );
+// }
 export class SliderComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.next = this.next.bind(this);
+    this.back = this.back.bind(this);
+  }
+  next() {
+    this.slider.slickNext();
+  }
+  back() {
+    this.slider.slickPrev();
+  }
   render() {
     const settings = {
       dots: true,
@@ -20,11 +46,11 @@ export class SliderComponent extends React.Component {
       speed: 500,
       slidesToShow: this.props.slidesPerView,
       slidesToScroll: this.props.slidesPerView,
-      nextArrow: <NextArrow />,
-      prevArrow: <PrevArrow />,
+      // nextArrow: <NextArrow />,
+      // prevArrow: <PrevArrow />,
       responsive: [
         {
-          breakpoint: 992,
+          breakpoint: 1200,
           settings: {
             slidesToShow: this.props.slidesPerView,
             slidesToScroll: this.props.slidesPerView,
@@ -33,7 +59,7 @@ export class SliderComponent extends React.Component {
           }
         },
         {
-          breakpoint: 768,
+          breakpoint: 992,
           settings: {
             slidesToShow: 2,
             slidesToScroll: 2,
@@ -41,7 +67,7 @@ export class SliderComponent extends React.Component {
           }
         },
         {
-          breakpoint: 576,
+          breakpoint: 768,
           settings: {
             slidesToShow: 1,
             slidesToScroll: 1
@@ -52,7 +78,7 @@ export class SliderComponent extends React.Component {
     return (
       <div className="row">
         <div className="col">
-          <Slider {...settings} className="row">
+          <Slider ref={c => (this.slider = c)} {...settings} className="row">
             {this.props.images.map((img_url, counter) => {
               return (
                 <img
@@ -64,6 +90,16 @@ export class SliderComponent extends React.Component {
               );
             })}
           </Slider>
+          <div className="row">
+            <div className="col">
+            <FontAwesomeIcon className="arrow-back bg-danger" icon={faArrowAltCircleLeft} onClick={this.back}/>
+            </div>
+            <div className="col text-center"> o o o</div>
+            <div className="col text-right">
+                {/* <FontAwesomeIcon className="arrow-next bg-success" icon={faArrowAltCircleRight} onClick={this.next}/> */}
+                <i className="bg-success far fa-arrow-alt-circle-right"></i>
+            </div>
+          </div>
         </div>
       </div>
     );
